8cc3ee31bf01928e9c6b54f77c38adeab7e5b46c
louis.jd.martin <louis.jd.martin@gmail.com>
2017-09-26 14:27:42 +0200

    Solde isibouffe pour ZZ

---
+++ espace_ZZ/solde_isibouffe/fonctions.php
@@ -0,0 +1,17 @@
+<?php
+ini_set("display_errors", "1");
+//script pour se connecter à la bdd et autres fonctions
+
+
+try
+{
+	$donnees = new PDO('mysql:host=localhost;dbname=bde;charset=utf8', 'bde', 'gkioXvQRD!L2');
+	
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+
+?>
