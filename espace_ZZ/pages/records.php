e23dc21a99c64dba567b72e5f5ae58dd2235eaf2
louis.jd.martin <louis.jd.martin@gmail.com>
2017-11-03 14:00:55 +0100

    Ajustement graphique tableau des scores

--- espace_ZZ/pages/records.php
+++ espace_ZZ/pages/records.php
@@ -1,10 +1,10 @@
 <?php
 	$scores = api("high_score", array("token" => $_SESSION['token']));
-	echo "<table>";
+	echo "<div style='overflow-x:auto;'><table class='overflowtable'>>";
 	echo "<tr>
 		<td>Article</td>
 		<td>Record</td>
-		<td>Score actuel</td>
+		<td><nobr>Score actuel</nobr></td>
 		<td>Classement</td>
 	</tr>";
 
@@ -12,13 +12,14 @@
 	foreach($scores as $s){
 		echo "
 		<tr>
-			<td>".$s['nom_art']."</td>
-			<td>".$s['nom_membre_record']." avec ".$s['record']." achats</td>
-			<td>Ton score: ".$s['score_actuel']."</td>
-			<td>Classement: ".$s['classement']."</td>
+			<td><nobr>".$s['nom_art']."</nobr></td>
+			<td><nobr>".$s['nom_membre_record']." avec ".$s['record']." achats</nobr></td>
+			<td><nobr>Ton score: ".$s['score_actuel']."</nobr></td>
+			<td><nobr>Classement: ".$s['classement']."</nobr></td>
 		</tr>
 		
 		";
 	}
-	echo "</table>";
+	echo "</table></div>";
 ?>
+<style>nobr{margin-right:16px;}</style>
