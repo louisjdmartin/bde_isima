029fb73f8e529a385988fa3dc07c7de4c4fa8814
louis.jd.martin <louis.jd.martin@gmail.com>
2017-12-05 09:25:30 +0100

    Ajout du cogomeme a l'easter egg

--- easter_eggs/cogonixkill/index.php
+++ easter_eggs/cogonixkill/index.php
@@ -1,5 +1,5 @@
 <title>You have been CogoniXkilled</title>
 <h1>You have been CogoniXkilled</h1>
 <?php
-	echo "<center><img src='".rand(1,9).".jpg'  /> </center>";
+	echo "<center><img src='".rand(1,10).".jpg'  /> </center>";
 ?>
