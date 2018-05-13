ee5f0cf1f01f515274aec730d61b8e8782602f03
louis.jd.martin <louis.jd.martin@gmail.com>
2018-05-13 19:17:37 +0200

    Amelioration systeme tombola

--- espace_ZZ/assets/php/print_tombola.php
+++ espace_ZZ/assets/php/print_tombola.php
@@ -5,8 +5,8 @@ echo "<body onload='window.print()'><meta charset='utf-8'>
 
 <style>
 	tr,td,table{border-collapse:collapse;border:1px solid black; padding:4px;text-align:center}
-	td{width:20%;padding:32px}
-	table{page-break-inside:avoid}
+	td{width:25%;padding:32px 0;}
+	table{page-break-inside:avoid;width:100%;}
 	.count{font-size:24px;}
 </style>
 <nobr><table> <tr>";
@@ -14,8 +14,10 @@ echo "<body onload='window.print()'><meta charset='utf-8'>
 $maxCols = 4;
 $counter = 0;
 $cols = 0;
-foreach($liste['liste'] as $l)
-	for($i=1;$i<=$l['qte'];$i++)
+foreach($liste['liste'] as $l){
+	$poids = 1;
+	if(isset($_GET['art'.$l['id_art']]) && is_numeric($_GET['art'.$l['id_art']]))$poids = $_GET['art'.$l['id_art']];
+	for($i=1;$i<=$l['qte']*$poids;$i++)
 	{
 		if(!empty($l['nom_membre']))$nom = $l['nom_membre'];
 		else $nom = $l['prenom']." ".$l['nom']; 
@@ -28,6 +30,6 @@ foreach($liste['liste'] as $l)
 		echo "<td><span class='count'>$counter </span><br /><span class='name'>$nom</span><br><em>".$l['nom_article']."</em></td>";
 
 	}
-
+}
 if($cols<$maxCols+1)for($i=$cols;$cols<$maxCols;$cols++)echo "<td>&nbsp;</td>";
 echo "</tr></table>";
