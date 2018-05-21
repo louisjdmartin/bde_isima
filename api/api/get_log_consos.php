d8453e42c139a00533c86f38eec5bf87284e91c2
louis.jd.martin <louis.jd.martin@gmail.com>
2018-05-21 14:18:06 +0200

    Correction bug isidroid

--- api/api/get_log_consos.php
+++ api/api/get_log_consos.php
@@ -32,7 +32,7 @@ function get_log_consos($settings, $objets){
 		$recharges = $bdd->query("SELECT transactions.timestamp, transactions.anciensolde, articles.nom, articles.tarif FROM transactions, articles, membres WHERE transactions.id_personne= membres.id AND membres.numero = ".$settings['numero']." AND articles.id=transactions.id_article ORDER BY timestamp DESC LIMIT 0, $limit");
 	}
 	else return array("error" => 1, "msg" => "Action refusé !", "nb_elt" => 0, "liste" => array());
-N	$retour = array();
+	$retour = array();
 	$retour["nb_elt"]=0;
 	foreach($recharges as $r){
 		$retour['liste'][] = array(
