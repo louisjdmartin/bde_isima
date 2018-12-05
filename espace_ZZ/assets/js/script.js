34c048065754c00c727c567ff2dd35cdd4d0b7fb
louis.jd.martin <louis.jd.martin@gmail.com>
2018-12-05 11:01:53 +0100

    Encore des meme de listeux

--- espace_ZZ/assets/js/script.js
+++ espace_ZZ/assets/js/script.js
@@ -107,8 +107,9 @@ function load(){
 	        "https://media.giphy.com/media/3osBLvEDEgSfQ7l3EY/giphy.gif",
 	        "https://bde.isima.fr/easter_eggs/cogonixkill/10.jpg", 
 	        "https://bde.isima.fr/easter_eggs/franck_romane.gif",
-		"https://bde.isima.fr/easter_eggs/memelisteux1.jpg",
-		"https://bde.isima.Fr/easter_eggs/memelisteux2.jpg"
+		"https://bde.isima.fr/easter_eggs/listeux/memelisteux1.jpg",
+		"https://bde.isima.fr/easter_eggs/listeux/memelisteux2.jpg",
+		"https://bde.isima.fr/easter_eggs/listeux/memelisteux3.jpg"
 	];
 	gn = getRandomInt(0,(gifs.length)-1);
 	$('.gif_load').html("<img src='"+gifs[gn]+"' alt='load' style='max-width:100%'/>");
