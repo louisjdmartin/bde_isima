6ef59d04f9b98138540400c82564762d3663813f
Hexabitt <lawrence.allardjacquin@neuf.fr>
2019-04-11 20:29:00 +0200

    Suppression dossier muzzic

--- muzzic/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$bddhost = "localhost";
-$bddlogin ="bde";
-$bddpass = "***REMOVED***";
-$bddname = "bde";
-
-$adminLogin = "BDE";
-$adminPass = "Fn9ZT0l1Q01CFg==";
-?>
--- muzzic/menu.php
+++
@@ -1,8 +0,0 @@
-<!-- La barre de navigation -->
-<table id="menu">
-<tr>
-  <td><a href="index.php">Voir les cr&eacute;neaux</a></td>
-  <td><a href="reserver.php">R&eacute;servez un cr&eacute;neau</a></td>
-  <td><a href="admin.php">Administration</a></td>
-</tr>
-</table>
